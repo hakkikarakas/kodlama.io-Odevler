95ac523e88cd476f940fd99116d7e958b0903591
hakkı karakaş <hakkikarks@gmail.com>
2021-08-16 19:07:43 +0300

    Tekrar -4.Gün 5.Ödev

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<Tk, Tv>
+    {
+        Tk[] _key;
+        Tv[] _value;
+        public MyDictionary()
+        {
+            _key = new Tk[0];
+            _value = new Tv[0];
+        }
+        public void Add(Tk key, Tv value)
+        {
+            Tv[] tempArray = _value;
+            Tk[] tempArray2 = _key;
+            _value = new Tv[tempArray.Length + 1];
+            _key = new Tk[tempArray2.Length + 1];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                _value[i] = tempArray[i];
+
+            }
+            for (int i = 0; i < tempArray2.Length; i++)
+            {
+                _key[i] = tempArray2[i];
+            }
+            _value[_value.Length - 1] = value;
+            _key[_key.Length - 1] = key;
+
+        }
+
+        public Tk[] Keys
+        {
+            get { return _key; }
+        }
+        public Tv[] Values
+        {
+            get { return _value; }
+        }
+        public int Count
+        {
+            get { return _key.Length; }
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int,string> myDictionary = new MyDictionary<int,string>();
+            myDictionary.Add(1,"Hakkı");
+            myDictionary.Add(2, "Karakaş");
+            myDictionary.Add(3, "İzmir");
+
+            for (int i = 0; i < myDictionary.Count; i++)
+            {
+                Console.WriteLine("Key: "+myDictionary.Keys[i]+"  "+"Value: "+myDictionary.Values[i]);
+            }
+        }
+    }
+   
+}
